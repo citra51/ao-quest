bc3989ad6caa4fdb7b886e2d8dc5a8df76982c0b
citra51 <citraksilala@gmail.com>
2024-05-24 12:25:35 +0700

    Fix wrong word

--- bot.lua
+++ bot.lua
@@ -75,23 +75,19 @@ function decideNextAction()
         ao.send({ Target = Game, Action = "PlayerAttack", Player = ao.id, AttackEnergy = tostring(player.energy) })
     elseif
         mychar.x == avoidUp then
-        print(colors.red ..
-        "We find the good direction, and avoid up to the center battle. " .. randomAvoidUpDirection .. colors.reset)
+        print(colors.red .. "We find the good direction, and avoid up to the center battle. " .. randomAvoidUpDirection .. colors.reset)
         ao.send({ Target = Game, Action = "PlayerMove", Player = ao.id, Direction = directionAvoidUp[randomIndexAvoidUp] })
     elseif
         mychar.x == avoidDown then
-        print(colors.red ..
-        "We find the good direction, and avoid down to the center battle. " .. randomAvoidDownDirection .. colors.reset)
+        print(colors.red .. "We find the good direction, and avoid down to the center battle. " .. randomAvoidDownDirection .. colors.reset)
         ao.send({ Target = Game, Action = "PlayerMove", Player = ao.id, Direction = directionAvoidDown[randomIndexAvoidDown] })
     elseif
         mychar.y == avoidLeft then
-        print(colors.red ..
-        "We find the good direction, and avoid down to the center battle. " .. randomAvoidLeftDirection .. colors.reset)
+        print(colors.red .. "We find the good direction, and avoid left to the center battle. " .. randomAvoidLeftDirection .. colors.reset)
         ao.send({ Target = Game, Action = "PlayerMove", Player = ao.id, Direction = directionAvoidLeft[randomIndexAvoidLeft] })
     elseif
         mychar.y == avoidRight then
-        print(colors.red ..
-        "We find the good direction, and avoid down to the center battle. " .. randomAvoidRightDirection .. colors.reset)
+        print(colors.red .. "We find the good direction, and avoid right to the center battle. " .. randomAvoidRightDirection .. colors.reset)
         ao.send({ Target = Game, Action = "PlayerMove", Player = ao.id, Direction = directionAvoidRight[randomIndexAvoidRight] })
     else
         print(colors.red .. "I dont know where am i, and where are our enemy move to " .. randomDirection .. colors.reset)
